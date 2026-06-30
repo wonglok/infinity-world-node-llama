68a5d7ad3ec71b1b831db154d86db20033e51697
Lok Lok <yellowhappy831@gmail.com>
2026-06-30 23:49:01 +0800

    electron building process notarise and sign

--- electron-builder.ts
+++ electron-builder.ts
@@ -1,11 +1,12 @@
 import path from "node:path";
-import {$} from "zx";
-import type {Configuration} from "electron-builder";
+import { $ } from "zx";
+import type { Configuration } from "electron-builder";
+import { handlerNota } from "./scripts/nota.mjs";
 
-const appId = "node-llama-cpp.electron.example";
-const productName = "node-llama-cpp Electron example";
-const executableName = "node-llama-cpp-electron-example";
-const appxIdentityName = "node.llama.cpp.electron.example";
+const appId = "infinity-ai.wonglok.com";
+const productName = "infinity-ai world";
+const executableName = "infinity-ai-electron";
+const appxIdentityName = "infinity-ai.wonglok.com";
 
 /**
  * @see - https://www.electron.build/configuration/configuration
@@ -16,20 +17,27 @@ export default {
     productName: productName,
     executableName: executableName,
     directories: {
-        output: "release"
+        output: "release",
     },
-    icon: "./public/app-icon.png",
+    icon: "./resource/icon.png",
 
     // remove this once you set up your own code signing for macOS
     async afterPack(context) {
         if (context.electronPlatformName === "darwin") {
+            console.log("begin sign");
             // check whether the app was already signed
-            const appPath = path.join(context.appOutDir, `${context.packager.appInfo.productFilename}.app`);
-
-            // this is needed for the app to not appear as "damaged" on Apple Silicon Macs
-            // https://github.com/electron-userland/electron-builder/issues/5850#issuecomment-1821648559
+            const appPath = path.join(
+                context.appOutDir,
+                `${context.packager.appInfo.productFilename}.app`,
+            );
+            // // this is needed for the app to not appear as "damaged" on Apple Silicon Macs
+            // // https://github.com/electron-userland/electron-builder/issues/5850#issuecomment-1821648559
             await $`codesign --force --deep --sign - ${appPath}`;
+            console.log("done sign");
         }
+    },
+    afterSign: async () => {
+        await handlerNota();
     },
     files: [
         "dist",
@@ -39,78 +47,68 @@ export default {
         "!node_modules/node-llama-cpp/llama/localBuilds/**/*",
         "node_modules/node-llama-cpp/llama/localBuilds/${os}-${arch}*/**/*",
         "!node_modules/@node-llama-cpp/*/bins/**/*",
-        "node_modules/@node-llama-cpp/${os}-${arch}*/bins/**/*"
+        "node_modules/@node-llama-cpp/${os}-${arch}*/bins/**/*",
     ],
     asarUnpack: [
         "node_modules/node-llama-cpp/bins",
         "node_modules/node-llama-cpp/llama/localBuilds",
-        "node_modules/@node-llama-cpp/*"
+        "node_modules/@node-llama-cpp/*",
     ],
     mac: {
-        target: [{
-            target: "dmg",
-            arch: [
-                "arm64",
-                "x64"
-            ]
-        }, {
-            target: "zip",
-            arch: [
-                "arm64",
-                "x64"
-            ]
-        }],
+        target: [
+            {
+                target: "dmg",
+                arch: ["arm64", "x64"],
+            },
+            {
+                target: "zip",
+                arch: ["arm64", "x64"],
+            },
+        ],
 
-        artifactName: "${name}.macOS.${version}.${arch}.${ext}"
+        artifactName: "${name}.macOS.${version}.${arch}.${ext}",
     },
     win: {
-        target: [{
-            target: "nsis",
-            arch: [
-                "x64",
-                "arm64"
-            ]
-        }],
+        target: [
+            {
+                target: "nsis",
+                arch: ["x64", "arm64"],
+            },
+        ],
 
-        artifactName: "${name}.Windows.${version}.${arch}.${ext}"
+        artifactName: "${name}.Windows.${version}.${arch}.${ext}",
     },
     appx: {
         identityName: appxIdentityName,
-        artifactName: "${name}.Windows.${version}.${arch}.${ext}"
+        artifactName: "${name}.Windows.${version}.${arch}.${ext}",
     },
     nsis: {
         oneClick: true,
         perMachine: false,
         allowToChangeInstallationDirectory: false,
-        deleteAppDataOnUninstall: true
+        deleteAppDataOnUninstall: true,
     },
     linux: {
-        target: [{
-            target: "AppImage",
-            arch: [
-                "x64",
-                "arm64"
-            ]
-        }, {
-            target: "snap",
-            arch: [
-                "x64"
-            ]
-        }, {
-            target: "deb",
-            arch: [
-                "x64",
-                "arm64"
-            ]
-        }, {
-            target: "tar.gz",
-            arch: [
-                "x64",
-                "arm64"
-            ]
-        }],
+        target: [
+            {
+                target: "AppImage",
+                arch: ["x64", "arm64"],
+            },
+            {
+                target: "snap",
+                arch: ["x64"],
+            },
+            {
+                target: "deb",
+                arch: ["x64", "arm64"],
+            },
+            {
+                target: "tar.gz",
+                arch: ["x64", "arm64"],
+            },
+        ],
         category: "Utility",
 
-        artifactName: "${name}.Linux.${version}.${arch}.${ext}"
-    }
+        artifactName: "${name}.Linux.${version}.${arch}.${ext}",
+    },
 } satisfies Configuration as Configuration;
---
+++ scripts/nota.mjs
@@ -0,0 +1,26 @@
+import { notarize } from '@electron/notarize'
+import path from 'path'
+
+// loaded file
+console.log('loaded: notarize')
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const handlerNota = async function () {
+  // context: any
+  console.log('notarize: start')
+  await notarize({
+    appPath: path.join(import.meta.dirname, '../dist/mac-arm64/hyperegg-ai.app'),
+    keychainProfile: 'WONG_LOK_PROFILE'
+
+    // add to bashrc or zshrc:
+    // APPLE_KEYCHAIN=login
+    // APPLE_KEYCHAIN_PROFILE=WONG_LOK_PROFILE
+  }).then((r) => {
+    console.log(r)
+    console.log('notarize: is ok!!')
+  })
+
+  return null
+}
+
+export default handlerNota
